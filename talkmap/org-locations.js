d5039fed7aea825e4796616883d19ff265217207
Anup-Deshmukh <danup1997@gmail.com>
2018-07-17 23:30:37 +0200

    misc

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,4 +1,31 @@
 var addressPoints = [
+  [
+    "Berkeley CA, USA",
+    37.8708393,
+    -122.2728638
+  ],
+  [
+    "London, UK",
+    51.5073219,
+    -0.1276473
+  ],
+
+  [
+     "Paris, Fr",
+     48.8566,
+     -2.3522 
+  ],
+
+  [
+    "San Francisco, California",
+    37.7792808,
+    -122.4192362
+  ],
+  [
+    "Los Angeles, CA",
+    34.0543942,
+    -118.2439408
+  ],
 
   [
     "Perth, Australia",
